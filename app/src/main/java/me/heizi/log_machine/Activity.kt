d1c72cc83bfabe6a9bab928be34f0309557115a4
Heizi <xm.zhongjiaxin@gmail.com>
2021-01-19 23:46:07 +0800

    init

---
+++ app/src/main/java/me/heizi/log_machine/Activity.kt
@@ -0,0 +1,11 @@
+package me.heizi.log_machine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_container)
+    }
+}
